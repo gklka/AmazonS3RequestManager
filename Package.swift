5137f18cbd21e9934a95c153838f9e60d65e6110
Gruber Kristóf <gk@lka.hu>
2022-09-13 10:38:29 +0200

    Added watchOS support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "AmazonS3RequestManager",
     platforms: [.macOS(.v10_15),
-                .iOS(.v13)],
+                .iOS(.v13),
+                .watchOS(.v7)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
